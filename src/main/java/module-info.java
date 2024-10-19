7905d1288dc5cb58822a367af6897fb95928093d
vrogerVascon <vrogerio87@gmail.com>
2024-10-19 08:50:57 -0300

    Local project created

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.course.javafxjdbc {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.course.javafxjdbc to javafx.fxml;
+    exports com.course.javafxjdbc;
+}
